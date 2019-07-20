cc64d67169a34bc6e85deb4c1c1d708aa24b0239
Kirsten Malcaba <kmalcaba@live.com>
2019-07-21 05:08:52 +0800

    AR .apk file

--- app/src/main/java/com/sample/kirsten/fitme/Img7.java
+++ app/src/main/java/com/sample/kirsten/fitme/Img7.java
@@ -8,6 +8,6 @@ public class Img7 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_img7);
+//        setContentView(R.layout.activity_img7);
     }
 }
